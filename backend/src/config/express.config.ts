6d0fce59c763603b6cb5a68515137b9182bf5d16
Jojo <sirjoandersonwf@gmail.com>
2022-01-28 20:01:03 -0300

    Inittial commit

---
+++ backend/src/config/express.config.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+
+import corsOptionsDelegate from './cors.config';
+import limiter from './limiter.config';
+
+//import { response } from '../middlewares/response.middleware.js';
+
+const app: express.Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static('uploads'));
+
+//app.use(cookieParser());
+app.use(cors(corsOptionsDelegate));
+app.use(limiter); // limiting all requests
+//app.use(response);
+
+export { app };
